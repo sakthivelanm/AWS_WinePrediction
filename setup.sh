e8247bbbe2fcdfb2c5b31c53a210978ec12cbf62
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-09 16:24:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,7 @@ sudo wget -O apache-spark.tgz "https://archive.apache.org/dist/spark/spark-3.2.0
 sudo mkdir -p /opt/spark
 sudo tar -xvzf apache-spark.tgz -C /opt/spark --strip-components=1
 sudo rm apache-spark.tgz
+
+sudo export SPARK_HOME=/opt/spark >> ~/.bashrc
+sudo export PATH=\$SPARK_HOME/bin:\$PATH >> ~/.bashrc
+sudo source ~/.bashrc
