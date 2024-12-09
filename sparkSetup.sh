f54f6dd5d81a3315aa5818194430428d5fba1dd6
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-09 18:25:37 -0500

    Update sparkSetup.sh

--- sparkSetup.sh
+++ sparkSetup.sh
@@ -13,10 +13,7 @@ sudo rm apache-spark.tgz
 echo "export SPARK_HOME=/opt/spark" >> ~/.bashrc
 echo "export PATH=\$SPARK_HOME/bin:\$PATH" >> ~/.bashrc
 
-sudo apt install python3-pip -y
-sudo apt install python3-numpy -y
-sudo apt install python3-pandas -y
-sudo apt install python3-quinn -y
-sudo apt install python3-pyspark -y
-sudo apt install python3-findspark -y
+sudo apt install libssl-dev -y
+sudo apt install python3 python3-pip -y
+sudo pip3 install dumpy pandas pyspark findspark quinn
 
