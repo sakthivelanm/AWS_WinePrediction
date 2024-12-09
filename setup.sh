119b221fa548b696ad3a6d53800bc6a41fc20e6c
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-09 16:06:37 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo mkdir /app
+sudo cp TrainingDataset.csv ValidationDataset.csv /app/
+sudo apt install openjdk-11-jdk -y
+sudo apt install python3-pip -y
+sudo wget -O apache-spark.tgz "https://archive.apache.org/dist/spark/spark-3.2.0/spark-3.2.0-bin-hadoop2.7.tgz"
+sudo mkdir -p /opt/spark
+sudo tar -xvzf apache-spark.tgz -C /opt/spark --strip-components=1
+sudo rm apache-spark.tgz
