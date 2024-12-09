5bf9b88e0508294e62ba391e82c394151a9648a1
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-09 18:49:50 -0500

    Update pythonSetup.sh

--- pythonSetup.sh
+++ pythonSetup.sh
@@ -2,6 +2,7 @@
 
 sudo apt install libssl-dev -y
 sudo apt install python3 python3-pip python3-venv -y
+python3 -m venv ~/myenv
 source ~/myenv/bin/activate
 sudo chown -R ubuntu:ubuntu ~/myenv
-pip install dumpy pandas pyspark quinn findspark
+pip install numpy pandas pyspark quinn findspark
