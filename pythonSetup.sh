17454d487317cce2da0ffa930390dd256f74d537
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-09 19:49:30 -0500

    Delete pythonSetup.sh

--- pythonSetup.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-sudo apt install libssl-dev -y
-sudo apt install python3 python3-pip python3-venv -y
-python3 -m venv ~/myenv
-source ~/myenv/bin/activate
-sudo chown -R ubuntu:ubuntu ~/myenv
-pip install numpy pandas pyspark quinn findspark
