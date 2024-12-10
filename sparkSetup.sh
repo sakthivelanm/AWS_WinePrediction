e44a8634c6818a7b37089d29bc78d4782fc9bffa
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-10 01:16:20 -0500

    Update sparkSetup.sh

--- sparkSetup.sh
+++ sparkSetup.sh
@@ -12,3 +12,5 @@ sudo rm apache-spark.tgz
 
 echo "export SPARK_HOME=/opt/spark" >> ~/.bashrc
 echo "export PATH=\$SPARK_HOME/bin:\$PATH" >> ~/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> ~/.bashrc
+echo "export PATH=$JAVA_HOME/bin:$PATH" >> ~/.bashrc
