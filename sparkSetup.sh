2558a8983a85056d4681c16e18e6ced9ea2554f3
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-09 20:34:36 -0500

    Update sparkSetup.sh

--- sparkSetup.sh
+++ sparkSetup.sh
@@ -3,7 +3,7 @@
 sudo apt update && sudo apt upgrade -y
 sudo mkdir /app
 sudo cp TrainingDataset.csv ValidationDataset.csv /app/
-sudo apt install openjdk-11-jdk -y
+sudo apt install -y deafult-jre
 sudo wget -O apache-spark.tgz "https://archive.apache.org/dist/spark/spark-3.2.0/spark-3.2.0-bin-hadoop2.7.tgz"
 sudo mkdir -p /opt/spark
 sudo tar -xvzf apache-spark.tgz -C /opt/spark --strip-components=1
