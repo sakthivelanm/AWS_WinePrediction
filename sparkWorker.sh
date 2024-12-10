232f48cd3e2a0c8d27593db3c0d025f8151a7945
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-10 01:54:05 -0500

    Update sparkWorker.sh

--- sparkWorker.sh
+++ sparkWorker.sh
@@ -3,7 +3,6 @@ export SPARK_MASTER_PORT=7077
 export SPARK_MASTER_WEBUI_PORT=8080
 export SPARK_LOG_DIR=/opt/spark/logs
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export SPARK_WORKER_LOG=/opt/spark/logs/spark-worker.out
 export SPARK_WORKER_WEBUI_PORT=8081
 export SPARK_WORKER_PORT=7000
 export SPARK_MASTER="spark://ip-172-31-22-218:7077"
@@ -15,4 +14,4 @@ sudo mkdir -p $SPARK_LOG_DIR
 sudo touch $SPARK_WORDER_LOG
 sudo ln -sf /dev/stdout $SPARK_WORKER_LOG
 cd /opt/spark/bin
-sudo ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG 2>&1 &
+sudo ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER
