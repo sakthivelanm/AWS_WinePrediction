38fd266eea8ee3a2f4b7947d7c839b2aa690aa21
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-10 01:01:05 -0500

    Update sparkWorker.sh

--- sparkWorker.sh
+++ sparkWorker.sh
@@ -2,17 +2,12 @@
 export SPARK_MASTER_PORT=7077
 export SPARK_MASTER_WEBUI_PORT=8080
 export SPARK_LOG_DIR=/opt/spark/logs
-export SPARK_MASTER_LOG=/opt/spark/logs/spark-master.out
-export JAVA_HOME=/usr/bin/java
-
-export SPARK_MASTER_PORT=7077 \
-export SPARK_MASTER_WEBUI_PORT=8080 \
-export SPARK_LOG_DIR=/opt/spark/logs \
-export SPARK_WORKER_LOG=/opt/spark/logs/spark-worker.out \
-export SPARK_WORKER_WEBUI_PORT=8080 \
-export SPARK_WORKER_PORT=7000 \
-export SPARK_MASTER="spark://ip-172-31-22-218:7077" \
-export SPARK_LOCAL_IP
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export SPARK_WORKER_LOG=/opt/spark/logs/spark-worker.out
+export SPARK_WORKER_WEBUI_PORT=8081
+export SPARK_WORKER_PORT=7000
+export SPARK_MASTER="spark://ip-172-31-22-218:7077"
+export SPARK_LOCAL_IP=$(hostname -I | awk '{print $1}')
 
 . "/opt/spark/bin/load-spark-env.sh"
 
@@ -20,4 +15,4 @@ sudo mkdir -p $SPARK_LOG_DIR
 sudo touch $SPARK_WORDER_LOG
 sudo ln -sf /dev/stdout $SPARK_WORKER_LOG
 cd /opt/spark/bin
-sudo ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
+sudo ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG 2>&1 &
