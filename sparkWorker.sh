e47ea851986a2020dc40a198303bcefd1dce4bdc
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-10 00:47:48 -0500

    Update sparkWorker.sh

--- sparkWorker.sh
+++ sparkWorker.sh
@@ -11,7 +11,7 @@ export SPARK_LOG_DIR=/opt/spark/logs \
 export SPARK_WORKER_LOG=/opt/spark/logs/spark-worker.out \
 export SPARK_WORKER_WEBUI_PORT=8080 \
 export SPARK_WORKER_PORT=7000 \
-export SPARK_MASTER="spark://ip-172-31-23-97:7077" \
+export SPARK_MASTER="spark://ip-172-31-22-218:7077" \
 export SPARK_LOCAL_IP
 
 . "/opt/spark/bin/load-spark-env.sh"
