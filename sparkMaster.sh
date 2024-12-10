9d4b300735fc88ea0340fbfb3bcdc598c6a79389
sakthivelanm <83674355+sakthivelanm@users.noreply.github.com>
2024-12-10 01:53:24 -0500

    Update sparkMaster.sh

--- sparkMaster.sh
+++ sparkMaster.sh
@@ -3,7 +3,6 @@
 export SPARK_MASTER_PORT=7077
 export SPARK_MASTER_WEBUI_PORT=8080
 export SPARK_LOG_DIR=/opt/spark/logs
-export SPARK_MASTER_LOG=/opt/spark/logs/spark-master.out
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 sudo mkdir -p $SPARK_LOG_DIR
@@ -13,4 +12,4 @@ sudo ln -sf /dev/stdout $SPARK_MASTER_LOG
 export SPARK_MASTER_HOST=`hostname`
 
 cd /opt/spark/bin &&
-sudo ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG 2>&1 &
+sudo ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
